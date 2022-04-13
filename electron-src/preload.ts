c5187cb4e20b79d51e1e33a7010d4b2c8ed515dd
insik-han <insikhan@itez.io>
2022-04-14 00:17:35 +0900

    first

--- electron-src/preload.ts
+++ electron-src/preload.ts
@@ -1,17 +1,13 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ipcRenderer, IpcRenderer } from 'electron'
+import { ipcRenderer, IpcRenderer } from "electron";
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      ipcRenderer: IpcRenderer
-    }
-  }
+  var ipcRenderer: IpcRenderer;
 }
 
 // Since we disabled nodeIntegration we can reintroduce
 // needed node functionality here
-process.once('loaded', () => {
-  global.ipcRenderer = ipcRenderer
-})
+process.once("loaded", () => {
+  global.ipcRenderer = ipcRenderer;
+});
